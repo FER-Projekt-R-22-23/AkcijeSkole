eceb17b2be3a892000ad2d4f5331d8cce9cfa2b7
lauraSeatovic <laura.seatovic@gmail.com>
2022-11-02 13:04:29 +0100

    Add post, put, delete, skole method

--- IzvidaciAkcijeSkole/AkcijeSkoleWebApi/Controllers/SkolaController.cs
+++ IzvidaciAkcijeSkole/AkcijeSkoleWebApi/Controllers/SkolaController.cs
@@ -38,6 +38,58 @@ namespace ExampleWebApi.Controllers
                 : NotFound();
         }
 
+        // PUT: api/Skole/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public IActionResult EditSkole(int id, Skola skola)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != skola.IdSkole)
+            {
+                return BadRequest();
+            }
+
+            if (!_skolaRepository.Exists(id))
+            {
+                return NotFound();
+            }
+
+            return _skolaRepository.Update(skola.toDbModel())
+                ? AcceptedAtAction("EditSkole", skola)
+                : StatusCode(500);
+        }
+
+        // POST: api/Skole
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public ActionResult<Skola> CreateRole(Skola skola)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return _skolaRepository.Insert(skola.toDbModel())
+                ? CreatedAtAction("GetSkole", new { id = skola.IdSkole }, skola)
+                : StatusCode(500);
+        }
+
+        // DELETE: api/Skole/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteRole(int id)
+        {
+            if (!_skolaRepository.Exists(id))
+                return NotFound();
+
+            return _skolaRepository.Remove(id)
+                ? NoContent()
+                : StatusCode(500);
+        }
+
 
     }
 }
