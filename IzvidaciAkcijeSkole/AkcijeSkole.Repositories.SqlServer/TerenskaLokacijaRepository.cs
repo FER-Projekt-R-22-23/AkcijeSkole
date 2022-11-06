771facddd19b70d38cc1125e05448f734e066a42
MarinKukucka <mk53435@fer.hr>
2022-11-07 00:18:16 +0100

    Add methods to repo and controller

--- IzvidaciAkcijeSkole/AkcijeSkole.Repositories.SqlServer/TerenskaLokacijaRepository.cs
+++ IzvidaciAkcijeSkole/AkcijeSkole.Repositories.SqlServer/TerenskaLokacijaRepository.cs
@@ -16,36 +16,139 @@ public class TerenskaLokacijaRepository : ITerenskaLokacijaRepository
 
     public bool Exists(TerenskaLokacija model)
     {
-        throw new NotImplementedException();
+        try
+        {
+            return _dbContext.TerenskeLokacije
+                        .AsNoTracking()
+                        .Contains(model.ToDbModel());
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 
     public bool Exists(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var model = _dbContext.TerenskeLokacije
+                            .AsNoTracking()
+                            .FirstOrDefault(terenskaLokacija => terenskaLokacija.IdTerenskeLokacije.Equals(id))
+            
+            return model is not null;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 
     public Result<TerenskaLokacija> Get(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var model = _dbContext.TerenskeLokacije
+                            .AsNoTracking()
+                            .FirstOrDefault(terenskaLokacija => terenskaLokacija.IdTerenskeLokacije.Equals(id))?
+                            .ToDomain();
+
+            return model is not null
+                ? Results.OnSuccess(model)
+                : Results.OnFailure<TerenskaLokacija>($"No Terenska lokacija with id {id} found);
+        }
+        catch (Exception e)
+        {
+            return Results.OnException<TerenskaLokacija>(e);
+        }
     }
 
     public Result<IEnumerable<TerenskaLokacija>> GetAll()
     {
-        throw new NotImplementedException();
+        try
+        {
+            var models = _dbContext.TerenskeLokacije
+                            .AsNoTracking()
+                            .Select(Mapping.ToDomain);
+
+            return Results.OnSuccess(models);
+        }
+        catch(Exception e)
+        {
+            return Results.OnException<IEnumerable<TerenskaLokacija>>(e);
+        }
     }
 
     public Result Insert(TerenskaLokacija model)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var dbModel = model.ToDbModel();
+            if(_dbContext.TerenskeLokacije.Add(dbModel).State == Microsoft.EntityFrameworkCore.EntityState.Added)
+            {
+                var isSuccess = _dbContext.SaveChanges() > 0;
+
+                _dbContext.Entry(dbModel).State = Microsoft.EntityFrameworkCore.EntityState.Detached;
+
+                return isSuccess
+                    ? Results.OnSuccess()
+                    : Results.OnFailure();
+            }
+
+            return Results.OnFailure();
+        }
+        catch(Exception e)
+        {
+            return Results.OnException(e);
+        }
     }
 
     public Result Remove(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var model = _dbContext.TerenskeLokacije
+                            .AsNoTracking()
+                            .FirstOrDefault(terenskaLokacija => terenskaLokacija.IdTerenskeLokacije.Equals(id));
+
+            if(model is not null)
+            {
+                _dbContext.TerenskeLokacije.Remove(model);
+
+                return _dbContext.SaveChanges() > 0
+                    ? Results.OnSuccess()
+                    : Results.OnFailure();
+            }
+            return Results.OnFailure();
+        }
+        catch(Exception e)
+        {
+            return Results.OnException(e);
+        }
     }
 
     public Result Update(TerenskaLokacija model)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var dbModel = model.ToDbModel();
+
+            if(_dbContext.TerenskeLokacije.Update(dbModel).State == Microsoft.EntityFrameworkCore.EntityState.Modified)
+            {
+                var isSuccess = _dbContext.SaveChanges() > 0;
+
+                _dbContext.Entry(dbModel).State = Microsoft.EntityFrameworkCore.EntityState.Detached;
+
+                return isSuccess
+                    ? Results.OnSuccess()
+                    : Results.OnFailure();
+            }
+
+            return Results.OnFailure();
+        }
+        catch(Exception e)
+        {
+            return Results.OnException(e);
+        }
     }
 }
--- IzvidaciAkcijeSkole/AkcijeSkoleWebApi/Controllers/TerenskaLokacijaController.cs
+++ IzvidaciAkcijeSkole/AkcijeSkoleWebApi/Controllers/TerenskaLokacijaController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AkcijeSkole.Repositories;
+using AkcijeSkoleWebApi.DTOs;
+using BaseLibrary;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AkcijeSkoleWebApi.Controllers;
 
@@ -6,5 +9,103 @@ namespace AkcijeSkoleWebApi.Controllers;
 [ApiController]
 public class TerenskaLokacijaController : ControllerBase
 {
+    private readonly ITerenskaLokacijaRepository _terenskaLokacijaRepository;
 
+    public TerenskaLokacijaController(ITerenskaLokacijaRepository terenskaLokacijaRepository)
+    {
+        _terenskaLokacijaRepository = terenskaLokacijaRepository;
+    }
+
+    // GET: api/TerenskaLokacija
+    [HttpGet]
+    public ActionResult<IEnumerable<TerenskaLokacija>> GetAllTerenskeLokacije()
+    {
+        var terenskaLokacijaResults = _terenskaLokacijaRepository.GetAll()
+            .Map(terenskaLokacija => terenskaLokacija.Select(DtoMapping.ToDto));
+
+        return terenskaLokacijaResults
+            ? Ok(terenskaLokacijaResults.Data)
+            : Problem(terenskaLokacijaResults.Message, statusCode: 500);
+    }
+
+    // GET: api/TerenskaLokacija/5
+    [HttpGet("{id}")]
+    public ActionResult<TerenskaLokacija> GetTerenskaLokacija(int id)
+    {
+        var terenskaLokacijaResult = _terenskaLokacijaRepository.Get(id).Map(DtoMapping.ToDto);
+
+        return terenskaLokacijaResult switch
+        {
+            { IsSuccess: true } => Ok(terenskaLokacijaResult.Data),
+            { IsFailure: true } => NotFound(),
+            { IsException: true } or _ => Problem(terenskaLokacijaResult.Message, statusCode: 500)
+        };
+    }
+
+    // PUT: api/TerenskaLokacija/5
+    [HttpPut("{id}")]
+    public IActionResult EditTerenskaLokacija(int id, TerenskaLokacija terenskaLokacija)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        if(id != terenskaLokacija.IdTerenskaLokacija)
+        {
+            return BadRequest();
+        }
+
+        if (!_terenskaLokacijaRepository.Exists(id))
+        {
+            return NotFound();
+        }
+
+        var domainTerenskaLokacija = terenskaLokacija.ToDomain();
+
+        var result = domainTerenskaLokacija.IsValid()
+                        .Bind(() => _terenskaLokacijaRepository.Update(domainTerenskaLokacija));
+
+        return result
+            ? AcceptedAtAction("EditTerenskaLokacija", terenskaLokacija)
+            : Problem(result.Message, statusCode: 500);
+    }
+
+    // POST: api/TerenskaLokacija
+    [HttpPost]
+    public ActionResult<TerenskaLokacija> CreateTerenskaLokacija(TerenskaLokacija terenskaLokacija)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var domainTerenskaLokacija = terenskaLokacija.ToDomain();
+
+        var validationResult = domainTerenskaLokacija.IsValid();
+        if (!validationResult)
+        {
+            return Problem(validationResult.Message, statusCode: 500);
+        }
+
+        var result = domainTerenskaLokacija.IsValid()
+                        .Bind(() => _terenskaLokacijaRepository.Insert(domainTerenskaLokacija));
+
+        return result
+            ? CreatedAtAction("GetTerenskaLokacija", new { id = terenskaLokacija.IdTerenskaLokacija }, terenskaLokacija)
+            : Problem(result.Message, statusCode: 500);
+    }
+
+    // DELETE: api/TerenskaLokacija/5
+    [HttpDelete("{id}")]
+    public IActionResult DeleteTerenskaLokacija(int id)
+    {
+        if (!_terenskaLokacijaRepository.Exists(id))
+            return NotFound();
+
+        var deleteResult = _terenskaLokacijaRepository.Remove(id);
+        return deleteResult
+            ? NoContent()
+            : Problem(deleteResult.Message, statusCode: 500);
+    }
 }
